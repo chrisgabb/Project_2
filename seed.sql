77cdb35369e61a3f24d311b497eaf6c9f745e41d
SendyMederos <sendymederos@hotmail.com>
2020-10-27 18:25:46 -0400

    members-pane.js get dinamiclly created elements css and main.html

--- seed.sql
+++ seed.sql
@@ -9,9 +9,9 @@ VALUES (3, "Sendy", "M", "senM", "2020-10-22T15:48:02.000", "2020-10-22T15:48:02
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
 VALUES (1, "CSS", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
-VALUES (2, "HTML", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
+VALUES (2, "HTML", "general", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
-VALUES (3, "Handlebars", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
+VALUES (3, "Handlebars", "focus", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
 VALUES (4, "ORM", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 

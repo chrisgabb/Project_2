a098573d65f39409bfc5fc19b6ef68e49eb61ccf
SendyMederos <sendymederos@hotmail.com>
2020-10-26 23:23:23 -0400

    s

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
+// const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
@@ -24,44 +23,7 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
+  // app.get("/members", isAuthenticated, (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/members.html"));
+  // });
 };
-=======
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-var path = require("path");
-
-// Routes
-// =============================================================
-module.exports = function(app) {
-
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  // cms route loads cms.html
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
-  });
-
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  // authors route loads author-manager.html
-  app.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/member-signup.html"));
-  });
-
-};
->>>>>>> 08581c35231c9a838e7169da7832507711191f9f

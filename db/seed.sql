ebd010375dbae335c6c186b7bb7835d3ec1a4b58
SendyMederos <sendymederos@hotmail.com>
2020-10-30 10:49:18 -0400

    sing up updates from chriss

--- db/seed.sql
+++ db/seed.sql
@@ -7,13 +7,13 @@ INSERT INTO members (id, first_name, last_name, userName, createdAt, updatedAt)
 VALUES (3, "Sendy", "M", "senM", "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000");
 
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
-VALUES (1, "CSS", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
+VALUES (1, "CSS", "general", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
-VALUES (2, "HTML", "general", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
+VALUES (2, "HTML", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
-VALUES (3, "Handlebars", "focus", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
+VALUES (3, "Handlebars", "general", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 INSERT INTO sessions (id, title, typeOfSession, info, membersLimit, timeStart, timeEnd, Opt1_timeStart, Opt1_timeEnd, Opt2_timeStart, Opt2_timeEnd, Opt3_timeStart, Opt3_timeEnd, createdAt, updatedAt)
-VALUES (4, "ORM", "request", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
+VALUES (4, "ORM", "focus", "I didnt get it", 5, "2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", null, null, null, null, null, null, "2020-10-22T15:48:02.000",  "2020-10-22T15:48:02.000");
 
 INSERT INTO session_member (createdAt, updatedAt, member_id, session_id)
 VALUES ("2020-10-22T15:48:02.000", "2020-10-22T15:48:02.000", 1, 1);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   // Getting references to the name input and author container, as well as the table body
   var firstNameInput = $("#member-first-name");
-  var lastNameInput = $("member-last-name")
-  var emailInput = $("member-email")
+  var lastNameInput = $("#member-last-name")
+  var emailInput = $("#member-email")
+  var userNameInput = $("#member-username")
+  var passwordInput = $("#member-password")
   // Adding event listeners to the form to create a new object, and the button to delete
   // an Author
-  $(document).on("submit", "#member-form", handleMemberFormSubmit);
-
-
+  $(document).on("submit", handleMemberFormSubmit);
   // A function to handle what happens when the form is submitted to create a new Author
   function handleMemberFormSubmit(event) {
     event.preventDefault();
-    // Don't do anything if the name fields hasn't been filled out
-    if (!firstNameInput.val().trim().trim() || !lastNameInput.val().trim().trim() || !emailInput.val().trim().trim()) {
+    // Don’t do anything if the name fields hasn’t been filled out
+    if (!firstNameInput.val().trim().trim() || !lastNameInput.val().trim().trim() || !emailInput.val().trim().trim() || !userNameInput.val().trim().trim() || !passwordInput.val().trim().trim()) {
       return;
     } else {
     // Calling the upsertAuthor function and passing in the value of the name input
@@ -25,15 +25,25 @@ $(document).ready(function() {
         .trim(),
       email: emailInput
         .val()
-        .trim()
-    
+        .trim(),
+      userName: userNameInput
+        .val()
+        .trim(),
+      password: passwordInput
+        .val()
+        .trim(),
     });
   }
   }
-
   // A function for creating a member. Calls getAuthors upon completion
-  function upsertMember(memberData) {
-    $.post("/api/members", memberData)
+  async function upsertMember(memberData) {
+    await $.post("/api/members", memberData)
+    console.log(memberData)
+    $.get("/api/members", function (data) {
+      memberId = data.length
+      console.log(memberId)
+      console.log(data[memberId])
+    })
+    //window.location = "http://localhost:8080/home"
   }
-
 });
